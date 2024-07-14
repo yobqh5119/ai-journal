10cc840c4f34afc579918efdc204b6f972310be8
yoobin <yoobin.hong@ingka.ikea.com>
2024-07-13 23:51:04 -0400

    landing page for the journal app

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">Your Journal App :)</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          Wonderful app for tracking your mood throughout your life. All you
+          have to do is be honest in your journal.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
